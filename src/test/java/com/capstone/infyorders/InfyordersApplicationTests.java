704352f4d35db30af9ca3cfdf86fc6ccefc285ba
P YOGESH <yogeshp0012@gmail.com>
2021-07-05 11:57:49 +0530

    Add files via upload

---
+++ src/test/java/com/capstone/infyorders/InfyordersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.infyorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfyordersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
